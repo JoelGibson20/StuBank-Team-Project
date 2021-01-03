8e66d34760a646c487f2bc4590256f080a923807
b9020460 <b9020460@newcastle.ac.uk>
2021-01-03 18:33:51 +0000

    On click methods added for all the buttons on home. They run when clicked, but no pages currently exists to rediret to on click

--- app/src/main/java/com/back4app/java/example/HomeScreen.java
+++ app/src/main/java/com/back4app/java/example/HomeScreen.java
@@ -2,8 +2,12 @@ package com.back4app.java.example;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.parse.FindCallback;
@@ -16,13 +20,22 @@ import com.back4app.java.example.ui.databaseMethods;
 
 public class HomeScreen extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
+        final ImageButton homeImageButton = findViewById(R.id.homeImageButton);
+        final ImageButton graphImageButton = findViewById(R.id.graphImageButton);
+        final ImageButton poundImageButton = findViewById(R.id.poundImageButton);
+        final ImageButton cardImageButton = findViewById(R.id.cardImageButton);
+        final ImageButton gearsImageButton = findViewById(R.id.gearsImageButton);
+
         final TextView greeting = findViewById(R.id.greeting);
         ParseObject userDetails = null;
+
 
         try {
             userDetails = databaseMethods.retrieveUserDetails(getIntent().getStringExtra("USERNAME"));
@@ -31,6 +44,27 @@ public class HomeScreen extends AppCompatActivity {
             e2.printStackTrace();
         }
         
+    }
+
+    public void homeButtonOnClick(View v){
+        System.out.println("test 1");
+        //Do something
+    }
+    public void graphButtonOnClick(View v){
+        System.out.println("test 2");
+        //Do something
+    }
+    public void poundButtonOnClick(View v){
+        System.out.println("test 3");
+        //Do something
+    }
+    public void cardButtonOnClick(View v){
+        System.out.println("test 4");
+        //Do something
+    }
+    public void gearsButtonOnClick(View v){
+        System.out.println("test 5");
+        //Do something
     }
 
 
