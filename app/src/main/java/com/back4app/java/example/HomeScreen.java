8089e1d3175f44d7d1c39687acfdffa481dd6126
b9020460 <b9020460@newcastle.ac.uk>
2021-01-05 16:29:26 +0000

    Example code of how to create cards via Java code. Will be used to create the scrollable section including user account cards.

--- app/src/main/java/com/back4app/java/example/HomeScreen.java
+++ app/src/main/java/com/back4app/java/example/HomeScreen.java
@@ -1,13 +1,20 @@
 package com.back4app.java.example;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.back4app.java.example.ui.card.CardActivity;
@@ -24,7 +31,7 @@ import com.back4app.java.example.ui.databaseMethods;
 import com.parse.ParseUser;
 
 public class HomeScreen extends AppCompatActivity {
-
+    int x = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +46,7 @@ public class HomeScreen extends AppCompatActivity {
         //Load the greeting text
         final TextView greeting = findViewById(R.id.greeting);
 
+
         ParseObject userDetails = null;
         //Gets the user's name to greet them by name
         greeting.setText(String.format("Welcome %s", databaseMethods.getCurrentUser().getString("firstName")));
@@ -46,7 +54,11 @@ public class HomeScreen extends AppCompatActivity {
         
     }
 
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     public void homeButtonOnClick(View v){
+        x += 50;
+        CreateMyCardView(x);
       /*Intent intent = new Intent(getApplicationContext(), HomeScreen.class);
       startActivity(intent);*/
         //Disabled for home screen as unnecessary
@@ -66,6 +78,57 @@ public class HomeScreen extends AppCompatActivity {
     public void gearsButtonOnClick(View v){
         Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
         startActivity(intent);
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    public void CreateMyCardView(int x){
+        System.out.println("X =" + x);
+        LinearLayout linearLayout = findViewById(R.id.linearLayout);
+        // Initialise the CardView
+        CardView cardview = new CardView(getApplicationContext());
+
+        // Create the “wrap_content” layout params that you’ll apply to the various
+        // UI elements we’re going to create
+        LinearLayout.LayoutParams layoutparams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        // Set the layoutParams on the CardView
+        cardview.setLayoutParams(layoutparams);
+
+        // Set the card’s corner radius
+        cardview.setRadius(6);
+
+        // Set its background color
+        cardview.setCardBackgroundColor(Color.GRAY);
+
+        // Set its maximum elevation
+        cardview.setMaxCardElevation(6);
+
+        cardview.setLeftTopRightBottom(400,x,50,0);
+
+        // Create a TextView
+        TextView textview = new TextView(getApplicationContext());
+
+        // Apply the layoutParams (wrap_content) to this TextView
+        textview.setLayoutParams(layoutparams);
+
+        // Define the text you want to display
+        textview.setText("Hello, World!");
+
+        // Define the text’s appearance, including its color
+        textview.setTextAppearance(android.R.style.TextAppearance_Material_Headline);
+        textview.setTextColor(Color.BLACK);
+
+
+        // Add the content to your CardView. Here, we’re adding the TextView//
+        cardview.addView(textview);
+
+
+        // Add the CardView to your layout
+        linearLayout.addView(cardview);
     }
 
 
