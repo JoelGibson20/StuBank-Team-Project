b61f864d4ee76e44ca8fd881167d276f00f050b1
b9018084 <b9018084@newcastle.ac.uk>
2021-01-10 21:10:52 +0000

    Transfer Confirmation Page created

--- app/src/main/java/com/back4app/java/example/NewRecipient.java
+++ app/src/main/java/com/back4app/java/example/NewRecipient.java
@@ -2,7 +2,11 @@ package com.back4app.java.example;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NewRecipient extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class NewRecipient extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_recipient);
+
+        Button submit = (Button) findViewById(R.id.sendbutton);
+        submit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), TransferComplete.class);
+                startActivity(intent);
+                String outgoingEmail = ((EditText) findViewById(R.id.emailInput)).getText().toString();
+                if (!outgoingEmail.equals("")) {
+
+                }
+            }
+        });
+
+
+
     }
+
+
+
 }
---
+++ app/src/main/java/com/back4app/java/example/TransferComplete.java
@@ -0,0 +1,30 @@
+package com.back4app.java.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.back4app.java.example.ui.pound.PoundActivity;
+
+public class TransferComplete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transfer_complete);
+        Button anotherTransfer = (Button) findViewById(R.id.anotherTransfer);
+        anotherTransfer.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PoundActivity.class);
+                startActivity(intent);
+            }});
+                Button returnHome = (Button) findViewById(R.id.returnHome);
+                returnHome.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View view) {
+                        Intent intent = new Intent(getApplicationContext(), HomeScreen.class);
+                        startActivity(intent);
+
+}});}}
