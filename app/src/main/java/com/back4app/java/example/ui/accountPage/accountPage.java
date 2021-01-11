bcb24852d3bc8067ac383a61bd43351deb395d46
b9020460 <b9020460@newcastle.ac.uk>
2021-01-11 17:12:12 +0000

    Account name, number, sort code, and balance are pulled from the account ParseObject and set on the accountPage. Need better formatting.

--- app/src/main/java/com/back4app/java/example/ui/accountPage/accountPage.java
+++ app/src/main/java/com/back4app/java/example/ui/accountPage/accountPage.java
@@ -3,6 +3,7 @@ package com.back4app.java.example.ui.accountPage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.back4app.java.example.R;
 import com.parse.ParseObject;
@@ -14,7 +15,18 @@ public class accountPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_page);
 
+        //Load the TextViews for modification
+        final TextView accountName = findViewById(R.id.accountName);
+        final TextView accountNumber = findViewById(R.id.accountNumber);
+        final TextView sortCode = findViewById(R.id.sortCode);
+        final TextView balance = findViewById(R.id.balance);
+
         //Retrieve the account in question from the intent
         ParseObject accountParseObject = (ParseObject) getIntent().getExtras().get("accountParseObject");
+
+        accountName.setText(accountParseObject.getString("accountName"));
+        accountNumber.setText(String.format("Account Number: %s",accountParseObject.getString("accountNumber")));
+        sortCode.setText(String.format("Sort Code: %s",accountParseObject.getString("sortCode")));
+        balance.setText(accountParseObject.getString("balance"));
     }
 }
