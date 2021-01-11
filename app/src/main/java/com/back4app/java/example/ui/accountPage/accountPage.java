7061d4adbfd0f32cd3d276c0fa394ba9a8f7026d
b9020460 <b9020460@newcastle.ac.uk>
2021-01-11 17:26:06 +0000

    Crated OnClickListeners for the buttons on the account page. They work, and methods can be implemented for each button now.

--- app/src/main/java/com/back4app/java/example/ui/accountPage/accountPage.java
+++ app/src/main/java/com/back4app/java/example/ui/accountPage/accountPage.java
@@ -2,10 +2,16 @@ package com.back4app.java.example.ui.accountPage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.back4app.java.example.HomeScreen;
 import com.back4app.java.example.R;
+import com.back4app.java.example.ui.databaseMethods;
+import com.parse.ParseException;
 import com.parse.ParseObject;
 
 public class accountPage extends AppCompatActivity {
@@ -21,6 +27,12 @@ public class accountPage extends AppCompatActivity {
         final TextView sortCode = findViewById(R.id.sortCode);
         final TextView balance = findViewById(R.id.balance);
 
+        //Load buttons to set onclick listeners for them
+        final Button renameButton = findViewById(R.id.renameButton);
+        final Button lockButton = findViewById(R.id.lockButton);
+        final Button statementButton = findViewById(R.id.statementButton);
+        final Button shareDetailsButton = findViewById(R.id.shareDetailsButton);
+
         //Retrieve the account in question from the intent
         ParseObject accountParseObject = (ParseObject) getIntent().getExtras().get("accountParseObject");
 
@@ -28,5 +40,36 @@ public class accountPage extends AppCompatActivity {
         accountNumber.setText(String.format("Account Number: %s",accountParseObject.getString("accountNumber")));
         sortCode.setText(String.format("Sort Code: %s",accountParseObject.getString("sortCode")));
         balance.setText(accountParseObject.getString("balance"));
+
+
+
+        renameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Rename button clicked");
+            }
+        });
+
+        lockButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Lock button clicked");
+            }
+        });
+
+        statementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Statement button clicked");
+            }
+        });
+
+        shareDetailsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Share Details button clicked");
+            }
+        });
+
     }
 }
