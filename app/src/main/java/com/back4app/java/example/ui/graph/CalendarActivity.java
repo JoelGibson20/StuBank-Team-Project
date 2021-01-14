77c9a3ee1bdc030c5baf85e43e2de0cfd98239f9
b9031371 <b9031371@newcastle.ac.uk>
2021-01-14 15:10:18 +0000

    Updated calendar page by adding a calendar function

--- app/src/main/java/com/back4app/java/example/ui/graph/CalendarActivity.java
+++ app/src/main/java/com/back4app/java/example/ui/graph/CalendarActivity.java
@@ -2,9 +2,14 @@ package com.back4app.java.example.ui.graph;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.back4app.java.example.HomeScreen;
@@ -15,16 +20,33 @@ import com.back4app.java.example.ui.settings.SettingsActivity;
 
 public class CalendarActivity extends GraphActivity {
 
+    private static final String TAG = "CalendarActivity";
+    private CalendarView calendarview;
+    TextView myDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        calendarview = (CalendarView) findViewById(R.id.calendarView);
+        myDate  = (TextView) findViewById(R.id.myDate);
+
+        calendarview.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = dayOfMonth + "/" + (month + 1) + "/" + year;
+                Log.d(TAG, "DateSelected: " + date);
+                myDate.setText(date);
+            }
+        });
 
         final ImageButton homeImageButton = findViewById(R.id.homeImageButton);
         final ImageButton graphImageButton = findViewById(R.id.graphImageButton);
         final ImageButton poundImageButton = findViewById(R.id.poundImageButton);
         final ImageButton cardImageButton = findViewById(R.id.cardImageButton);
         final ImageButton gearsImageButton = findViewById(R.id.gearsImageButton);
+        final Button cancelbutton = findViewById(R.id.button_cancel);
+
 
     }
     public void homeButtonOnClick(View v){
@@ -47,4 +69,10 @@ public class CalendarActivity extends GraphActivity {
         Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
         startActivity(intent);
     }
+
+    public void cancelbuttonOnClick(View v){
+        Intent intent = new Intent(getApplicationContext(), GraphActivity.class);
+        startActivity(intent);
+    }
+
 }
