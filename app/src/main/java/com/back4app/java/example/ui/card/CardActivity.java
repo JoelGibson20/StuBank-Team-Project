02bdaa6f16b5e791635c5752c3833935a8973603
b8040617 <p.gronostajski@newcastle.ac.uk>
2021-01-16 01:29:51 +0000

    clicking show card details button will now display them in an alert dialog; added an option to copy the card number to clipboard

--- app/src/main/java/com/back4app/java/example/ui/card/CardActivity.java
+++ app/src/main/java/com/back4app/java/example/ui/card/CardActivity.java
@@ -1,25 +1,56 @@
 package com.back4app.java.example.ui.card;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+
 
 import com.back4app.java.example.HomeScreen;
 import com.back4app.java.example.R;
 import com.back4app.java.example.ui.graph.GraphActivity;
 import com.back4app.java.example.ui.pound.PoundActivity;
+
 import com.back4app.java.example.ui.settings.SettingsActivity;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 
 public class CardActivity extends AppCompatActivity {
+    final String[] cardNum = new String[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card);
+
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Cards");
+        // The query will search for a ParseObject, given its objectId.
+        // When the query finishes running, it will invoke the GetCallback
+        // with either the object, or the exception thrown
+        query.getInBackground("HXVXy0xBT3", new GetCallback<ParseObject>() {
+            public void done(ParseObject result, ParseException e) {
+                if (e == null) {
+                    cardNum[0] = result.getString("cardNumber");
+                    cardNum[1] = result.getString("cvv");
+                    cardNum[2] = String.valueOf(result.getString("expiryDate"));
+                }
+            }
+        });
 
         final ImageButton homeImageButton = findViewById(R.id.homeImageButton);
         final ImageButton graphImageButton = findViewById(R.id.graphImageButton);
@@ -28,6 +59,7 @@ public class CardActivity extends AppCompatActivity {
         final ImageButton gearsImageButton = findViewById(R.id.gearsImageButton);
 
     }
+
     public void homeButtonOnClick(View v){
         Intent intent = new Intent(getApplicationContext(), HomeScreen.class);
         startActivity(intent);
@@ -41,11 +73,38 @@ public class CardActivity extends AppCompatActivity {
         startActivity(intent);
     }
     public void cardButtonOnClick(View v){
-/*        Intent intent = new Intent(getApplicationContext(), CardActivity.class);
-        startActivity(intent);*/
+//        Intent intent = new Intent(getApplicationContext(), createCard.class);
+//        startActivity(intent);
+    }
+    public void addCardButtonOnClick(View v){
+        Intent intent = new Intent(getApplicationContext(), createCard.class);
+        startActivity(intent);
     }
     public void gearsButtonOnClick(View v){
         Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
         startActivity(intent);
+    }
+    public void showCardDetailsOnClick(View v){
+        Button cardDetailsButton = findViewById(R.id.cardDetails);
+        cardDetailsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(CardActivity.this);
+
+                builder.setCancelable(true);
+                builder.setTitle("These are your card details:");
+                builder.setMessage("Card Number: " + cardNum[0] + "\n" +"CVV: " + cardNum[1] + "\n" +"Expiry Date: "+ cardNum[2]);
+
+                builder.setPositiveButton("Copy Card Number to Clipboard", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                        ClipData clip = ClipData.newPlainText("card number", cardNum[0]);
+                        clipboard.setPrimaryClip(clip);
+                    }
+                });
+                builder.show();
+            }
+        });
     }
 }
