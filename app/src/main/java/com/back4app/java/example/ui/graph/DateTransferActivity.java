e5c8054dff63b1d6ea9f52914992b31bdbe87477
b9031371 <b9031371@newcastle.ac.uk>
2021-01-18 15:21:21 +0000

    tried retrieving data from the transaction table

--- app/src/main/java/com/back4app/java/example/ui/graph/DateTransferActivity.java
+++ app/src/main/java/com/back4app/java/example/ui/graph/DateTransferActivity.java
@@ -2,28 +2,69 @@ package com.back4app.java.example.ui.graph;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.back4app.java.example.R;
+import com.parse.GetCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.sql.Ref;
 
 public class DateTransferActivity extends CalendarActivity {
+    private static final String TAG = "DateTransferActivity";
+
+
+    TextView transactionvalue;
+    TextView transactionreference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datetransfer);
 
-
         final Button backbutton = findViewById(R.id.backbutton);
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Transactions");
+        query.whereEqualTo("objectId", "RdTPBzuFrs");
+        query.getFirstInBackground((transaction, e) -> {
+            if (e == null) {
+                String Value = transaction.getString("value");
+                String Reference = transaction.getString("reference");
+                transactionreference.setText(Reference);
+                transactionvalue.setText(Value);
+                Log.d(TAG, "transaction: " + Reference);
+                Log.d(TAG, "Amount: " + Value);
+
+            }else{
+                Log.d(TAG, "error");
+            }
+        });
 
 
 
     }
 
+
+
+
     public void backbuttonOnClick(View v) {
         Intent intent = new Intent(getApplicationContext(), CalendarActivity.class);
         startActivity(intent);
 
+    }
 
-    }}
+    //const Transactions = Parse.Object.extent("Transactions");
+    //const transactions = new Parse.Query
+
+
+    }
+
+
+
