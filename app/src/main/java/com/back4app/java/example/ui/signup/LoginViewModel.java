7617c50dbcb7de52d1f9fa35a3ecf51cdef68811
b9020460 <b9020460@newcastle.ac.uk>
2021-01-24 16:23:39 +0000

    Added regex expression to check that the password provided on signup is at least 8 characters including uppercase, lowercase, digit, and special character. Error message changed to reflect this.

--- app/src/main/java/com/back4app/java/example/ui/signup/LoginViewModel.java
+++ app/src/main/java/com/back4app/java/example/ui/signup/LoginViewModel.java
@@ -72,6 +72,6 @@ public class LoginViewModel extends ViewModel {
 
     // A placeholder password validation check
     private boolean isPasswordValid(String password) {
-        return password != null && password.trim().length() > 5;
+        return password != null && password.matches("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$");
     }
 }
