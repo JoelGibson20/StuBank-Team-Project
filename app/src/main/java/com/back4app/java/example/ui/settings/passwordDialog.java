614aafe74b726d14ded6eb3e2d51644862653af3
b9031371 <b9031371@newcastle.ac.uk>
2021-01-25 18:48:42 +0000

    Made the layout for the settings page and created a pop up when change password button is clicked and started validation

--- app/src/main/java/com/back4app/java/example/ui/settings/passwordDialog.java
+++ app/src/main/java/com/back4app/java/example/ui/settings/passwordDialog.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -14,11 +15,17 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
 import com.back4app.java.example.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class passwordDialog extends AppCompatDialogFragment {
     private EditText editTextTypePassword;
     private EditText editTextRetypePassword;
     private  passwordDialogListener listener;
+    private TextInputLayout textInputPassword1;
+    private  TextInputLayout textInputPassword2;
+
+    private static final String TAG = "PasswordDialog";
+
 
     @NonNull
     @Override
@@ -27,6 +34,12 @@ public class passwordDialog extends AppCompatDialogFragment {
 
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.layout_dialog, null);
+
+        editTextTypePassword = view.findViewById(R.id.type_password);
+        editTextRetypePassword = view.findViewById(R.id.retype_password);
+
+        textInputPassword1 = view.findViewById(R.id.type_password_layout);
+        textInputPassword2 = view.findViewById(R.id.retype_password_layout);
 
         builder.setView(view)
                 .setTitle("Change Password")
@@ -39,18 +52,52 @@ public class passwordDialog extends AppCompatDialogFragment {
                 .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        String password1 = editTextTypePassword.getText().toString();
-                        String password2 = editTextRetypePassword.getText().toString();
-                        listener.applyTexts(password1, password2);
+                        if(!validatePassword() | !validatePassword2()){
+                            String password1 = editTextTypePassword.getText().toString();
+                            String password2 = editTextRetypePassword.getText().toString();
+                            listener.applyTexts(password1, password2);
+                        }
                     }
                 });
 
-        editTextTypePassword = view.findViewById(R.id.type_password);
-        editTextRetypePassword = view.findViewById(R.id.retype_password);
+
+
 
         return builder.create();
 
     }
+
+
+    private boolean validatePassword(){
+        String passwordInput = editTextRetypePassword.getText().toString();
+
+        if(passwordInput.isEmpty()){
+            textInputPassword1.setError("Please input new Password");
+            Log.d(TAG, "PLEASE INPUT NEW PASSWORD");
+            return false;
+        }else{
+            textInputPassword1.setError(null);
+            Log.d(TAG, "RETURNS TRUE");
+            return true;
+        }
+
+    }
+
+    private boolean validatePassword2(){
+        String passwordInput = editTextTypePassword.getText().toString();
+
+        if(passwordInput.isEmpty()){
+            textInputPassword2.setError("Please input new Password");
+            return false;
+        }else{
+            textInputPassword2.setError(null);
+            return true;
+        }
+
+    }
+
+
+
 
     @Override
     public void onAttach(@NonNull Context context) {
